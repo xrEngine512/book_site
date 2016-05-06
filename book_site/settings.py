47ae078e8061e76a4cb9565c5137a5bb114c521c
islam <g.man.kbr@gmail.com>
2016-05-07 00:00:31 +0300

    database mysql setup

--- book_site/settings.py
+++ book_site/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'book_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'site_db',
+        'USER': 'admin',
+        'PASSWORD': 'default_password',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
