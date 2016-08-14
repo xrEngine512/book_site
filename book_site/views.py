a70a00938bef27941321866389fc4f16e6575efa
islam <g.man.kbr@gmail.com>
2016-08-14 16:58:00 +0300

    Blog pre alpha

--- book_site/views.py
+++ book_site/views.py
@@ -4,7 +4,3 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
-
-
-def test_request(request):
-    return JsonResponse({"data": "Hurrah!!!"})
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
+def blog_entries(request):
+    entries = [
+        {
+            'header': 'Ну и чего...',
+            'description': 'Я очень люблю нейросети, но такого дерьма про них я давно не читал. Как я мог такое написать...',
+            'author': {'name': 'Никишечкин (neuroPRO) Анатолий'},
+            'creationDate': {'isoFormat': '2016-08-14', 'humanReadable': '18 авг 2016'}
+        }
+    ] * 6
+
+    print('got request!')
+
+    return JsonResponse({'blog_entries': entries})
