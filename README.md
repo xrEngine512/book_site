b176dd64e5e258bc66b235a0c631f78ad1610e96
Arsen <arsen.utepbergenov@yandex.ru>
2016-11-18 13:14:34 +0300

    small update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# npm install -g gulp
-# npm install --save-dev gulp
-# npm install --save-dev gulp-concat
-# npm install --save-dev gulp-uncss
-# npm install --save-dev gulp-uglify
-# npm gulp scripts
+npm install -g gulp
+npm install --save-dev gulp
+npm install --save-dev gulp-concat
+npm install --save-dev gulp-uncss
+npm install --save-dev gulp-uglify
+npm gulp scripts
--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,25 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     uncss = require('gulp-uncss');
 
-// test task
+// неправильно работает в модальных окнах
+/*
+gulp.task('css', function() {
+    return gulp.src('static/libs/bootstrap.min.css')
+        .pipe(uncss({
+            html: ['index.html', 'static/comments/*.html', 'static/qna/*.html',
+                   'static/pages/home.html', 'static/pages/store.html', 'static/pages/contact.html', 'static/pages/about-us.html',
+                   'static/pages/product-details.html', 'static/pages/blog.html', 'static/pages/blog-details.html']
+        }))
+        .pipe(gulp.dest('static/public/css/'));
+});*/
+
 gulp.task('scripts', function() {
     return gulp.src(['static/qna/qna.module.js', 'static/qna/qna.component.js',
-                     'static/comments/comments.module.js', 'static/comments/comments.component.js',
-                     'static/js/*.js'])
-    .pipe(concat('scripts.js'))
-    .pipe(gulp.dest('static/public/js'));
+            'static/comments/comments.module.js', 'static/comments/comments.component.js',
+            'static/js/*.js'
+        ])
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('static/public/js'));
 });
+
+gulp.task('build', ['scripts']);
