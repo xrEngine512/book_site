5ddc2f835034da36ea306948ef48ea4a7190cf67
islam <g.man.kbr@gmail.com>
2017-01-05 03:47:56 +0300

    bug fix in configuration settings

--- book_site/settings.py
+++ book_site/settings.py
@@ -30,7 +30,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'rest_framework',
     'common',
     'blog',
