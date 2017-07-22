a38bdfa8c0526317ad8dd79faf6184fb33ceabdd
Arsen <arsen.utepbergenov@yandex.ru>
2017-07-22 22:18:54 +0300

    update styles blog

--- static/js/blog.js
+++ static/js/blog.js
@@ -1,5 +1,6 @@
 gApp.controller('blogController', function($scope, $http, Blog) {
     Blog.query({}, function(data) {
         $scope.blog_entries = data;
+        console.log($scope.blog_entries );
     });
 });
